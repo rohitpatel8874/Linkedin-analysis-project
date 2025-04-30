baf8abd06b63c88b66452236f4a81664b55128e9
Areeba Shakeel <ahmadshakeel5918@gmail.com>
2025-04-30 09:47:07 +0530

    ..

--- app.py
+++ app.py
@@ -86,77 +86,10 @@ def register():
         return redirect(url_for('login'))
     return render_template('Registration.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
-
-#Graphs functions
-
-#common job titles
-def common_job_titles():
-    fig1 = go.Figure()
-    top_titles = df['title'].value_counts().nlargest(15).reset_index()
-    top_titles.columns = ['Job Title', 'Count']
-
-    fig1 = px.bar(top_titles, x='Job Title', y='Count',
-              title="Top 15 Most Common Job Titles",
-              color='Count', text='Count')
-    fig1.update_layout(xaxis_tickangle=-45)
-
-    graph1_html = pio.to_html(fig1, full_html=False)
-    return graph1_html
-
-# Distribution of Employment Types
-def employment_type_distribution():
-    fig2 = go.Figure()
-    employment_dist = df['Employment type'].value_counts().reset_index()
-    employment_dist.columns = ['Employment Type', 'Count']
-
-    fig2 = px.pie(employment_dist, names='Employment Type', values='Count',
-              title="Distribution of Employment Types",
-              hole=0.3)
-    graph2_html = pio.to_html(fig2, full_html=False)
-    return graph2_html   # Donut style
-
-
-# Distribution of Seniority Levels
-def seniority_level_distribution():
-    fig3 = go.Figure()
-    fig3 = px.histogram(df, x='Seniority level',
-                    title="Distribution of Seniority Levels",
-                    color='Seniority level')
-    fig3.update_layout(xaxis_title='Seniority Level', yaxis_title='Count')
-    graph3_html = pio.to_html(fig3, full_html=False)
-    return graph3_html   # Donut style
-
-   
-
-
-# Seniority Level by Employment Type
-def seniority_level_by_employment_type():
-    fig4 = go.Figure()
-    combo = df.groupby(['Employment type', 'Seniority level']).size().reset_index(name='Count')
-
-    fig4 = px.bar(combo, x='Employment type', y='Count', color='Seniority level',
-              title="Seniority Level by Employment Type",
-              barmode='stack')
-    graph4_html = pio.to_html(fig4, full_html=False)
-    return graph4_html   # Donut style
-
-
-
-# Top 10 Companies by Job Postings
-def top_companies():
-    fig5 = go.Figure()  
-    top_companies = df['company'].value_counts().nlargest(10).reset_index()
-    top_companies.columns = ['Company', 'Postings']
-
-    fig5 = px.pie(top_companies, names='Company', values='Postings',
-                title="Top 10 Companies by Job Postings",
-                hole=0.4)
-    graph5_html = pio.to_html(fig5, full_html=False)
-    return graph5_html   # Donut style
-
-
-#analysis page routes
 @app.route('/job_analysis')
 def job_analysis():
     # Generate the graphs
@@ -167,6 +100,208 @@ def job_analysis():
     graph5_html = top_companies()
     # Add more graphs as needed
     return render_template('job_analysis.html', graph1_html=graph1_html, graph2_html=graph2_html, graph3_html=graph3_html, graph4_html=graph4_html, graph5_html=graph5_html)
+
+
+
+
+
+#Graphs functions
+
+#common job titles
+def common_job_titles():
+    top_titles = df['title'].value_counts().nlargest(15).reset_index()
+    top_titles.columns = ['Job Title', 'Count']
+
+    fig1 = px.bar(top_titles, 
+                  x='Count', 
+                  y='Job Title',
+                  orientation='h',
+                  title="Top 15 Most Common Job Titles",
+                  color='Count',
+                  color_continuous_scale='blues',
+                  text='Count')
+    
+    fig1.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        title={
+            'font_size': 24,
+            'xanchor': 'center',
+            'x': 0.5
+        },
+        yaxis=dict(
+            title='',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        xaxis=dict(
+            title='Number of Positions',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        showlegend=False,
+        height=600
+    )
+    
+    fig1.update_traces(
+        textposition='outside',
+        textfont=dict(size=12),
+        hovertemplate="<b>%{y}</b><br>Positions: %{x}<extra></extra>"
+    )
+
+    graph1_html = pio.to_html(fig1, full_html=False)
+    return graph1_html
+
+# Distribution of Employment Types
+def employment_type_distribution():
+    employment_dist = df['Employment type'].value_counts().reset_index()
+    employment_dist.columns = ['Employment Type', 'Count']
+
+    fig2 = px.pie(employment_dist, names='Employment Type', values='Count',
+                title="Distribution of Employment Types",
+                hole=0.3) 
+
+    graph2_html = pio.to_html(fig2, full_html=False)
+    return graph2_html
+
+# Distribution of Seniority Levels
+def seniority_level_distribution():
+    seniority_dist = df['Seniority level'].value_counts().reset_index()
+    seniority_dist.columns = ['Seniority Level', 'Count']
+    
+    fig3 = px.bar(seniority_dist,
+                  x='Seniority Level',
+                  y='Count',
+                  title="Distribution of Seniority Levels",
+                  color='Count',
+                  color_continuous_scale='blues',
+                  text='Count')
+    
+    fig3.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        title={
+            'font_size': 24,
+            'xanchor': 'center',
+            'x': 0.5
+        },
+        xaxis=dict(
+            title='',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        yaxis=dict(
+            title='Number of Positions',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        showlegend=False,
+        height=500
+    )
+    
+    fig3.update_traces(
+        textposition='outside',
+        textfont=dict(size=12),
+        hovertemplate="<b>%{x}</b><br>Positions: %{y}<extra></extra>"
+    )
+
+    graph3_html = pio.to_html(fig3, full_html=False)
+    return graph3_html
+
+# Seniority Level by Employment Type
+def seniority_level_by_employment_type():
+    combo = df.groupby(['Employment type', 'Seniority level']).size().reset_index(name='Count')
+    
+    colors = px.colors.sequential.Blues[2:]
+    
+    fig4 = px.bar(combo, 
+                  x='Employment type', 
+                  y='Count', 
+                  color='Seniority level',
+                  title="Seniority Level Distribution by Employment Type",
+                  barmode='stack',
+                  color_discrete_sequence=colors,
+                  text='Count')
+    
+    fig4.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        title={
+            'font_size': 24,
+            'xanchor': 'center',
+            'x': 0.5
+        },
+        xaxis=dict(
+            title='Employment Type',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        yaxis=dict(
+            title='Number of Positions',
+            tickfont=dict(size=12),
+            gridcolor='lightgray'
+        ),
+        legend=dict(
+            title='Seniority Level',
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+        height=500
+    )
+    
+    fig4.update_traces(
+        textposition='inside',
+        textfont=dict(size=10, color='white'),
+        hovertemplate="<b>%{x}</b><br>%{data.name}<br>Positions: %{y}<extra></extra>"
+    )
+
+    graph4_html = pio.to_html(fig4, full_html=False)
+    return graph4_html
+
+# Top 10 Companies by Job Postings
+def top_companies():
+    top_companies = df['company'].value_counts().nlargest(10).reset_index()
+    top_companies.columns = ['Company', 'Postings']
+    
+    colors = px.colors.sequential.Blues[2:]
+    
+    fig5 = px.pie(top_companies, 
+                  names='Company', 
+                  values='Postings',
+                  title="Top 10 Companies by Job Postings",
+                  hole=0.6,
+                  color_discrete_sequence=colors)
+    
+    fig5.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        title={
+            'font_size': 24,
+            'xanchor': 'center',
+            'x': 0.5
+        },
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=-0.5,
+            xanchor="center",
+            x=0.5
+        ),
+        annotations=[dict(text='Top 10<br>Companies', x=0.5, y=0.5, font_size=20, showarrow=False)],
+        height=600
+    )
+    
+    fig5.update_traces(
+        textposition='outside',
+        textinfo='percent+label',
+        hovertemplate="<b>%{label}</b><br>Positions: %{value}<br>Percentage: %{percent}<extra></extra>"
+    )
+
+    graph5_html = pio.to_html(fig5, full_html=False)
+    return graph5_html
 
 
 
